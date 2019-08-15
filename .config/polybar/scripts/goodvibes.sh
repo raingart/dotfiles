68eaf71dfd54e60d8609c949e98abf7da166a326
null <raingart@users.noreply.github.com>
2019-08-15 21:56:19 +0300

    current

--- .config/polybar/scripts/goodvibes.sh
+++ .config/polybar/scripts/goodvibes.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-player_track_name=$(qdbus org.mpris.MediaPlayer2.Goodvibes /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlaybackStatus)
+goodvibes_status=$(qdbus org.mpris.MediaPlayer2.Goodvibes /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlaybackStatus 2>/dev/null)
+player_track_name=''
 
-# show_play_track() {
-  if [[ $player_track_name == "Playing" ]];then
-      echo $(qdbus org.mpris.MediaPlayer2.Goodvibes /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Metadata | grep "^xesam:title:" | cut -d':' -f3-)
-  else
-      echo ''
-  fi
+if [[ $(pidof goodvibes 2>/dev/null) && "$goodvibes_status" == "Playing" ]];then
+   player_track_name=$(qdbus org.mpris.MediaPlayer2.Goodvibes /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Metadata | grep "^xesam:title:" | cut -d':' -f3-)
+fi
+
+echo $player_track_name
